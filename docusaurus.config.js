00077e535a2e0c1e99caadd072dabda628b2c8e2
BÄDASS <111883156+GitBADASS@users.noreply.github.com>
2024-08-13 20:45:21 +0800

    Initialize the website

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'GitBADASS.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'GitBADASS', // Usually your GitHub org/user name.
+  projectName: 'GitBADASS.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
